56664f91fb0198925dfaa94886380ff47cd24760
samirsharfi <boomsharfi@gmail.com>
2022-11-28 21:04:55 +0800

    prepare deploy

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
+const path = require("path");
 const express = require("express");
-const colors = require('colors')
+const colors = require("colors");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv").config();
-const port =  5000;
+const port = 5000;
 
-connectDB()
+connectDB();
 
 const app = express();
 //the request we wanna listen for
@@ -20,6 +21,21 @@ app.use(express.urlencoded({ extended: false }));
 //instead of use endpoint use / if you hit /api/goals it will look to this file
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
+
+//Serve frontend
+if (process.env.NODE_ENV === "production") {
+  //Set static folder (build folder for react)
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  //our route
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+    app.get('/', (req, res)=> res.send('Please set to production'))
+}
 
 //overwrite the default Express error
 app.use(errorHandler);
